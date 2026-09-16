49e932ab34d7994e3eda51f14e4b1db0b357323c
semawon okedairo <okedairosemawon@gmail.com>
2026-09-16 20:54:12 +0100

    fixed syntax error

--- BankApi.Domain/ValueObjects/Money.cs
+++ BankApi.Domain/ValueObjects/Money.cs
@@ -1,17 +1,17 @@
 namespace BankApi.Domain.ValueObjects;
 
-public record Money 
+public record Money
 {
-    public decimal Amount {get; init;}
-    public string Currency {get; init;}
+    public decimal Amount { get; init; }
+    public string Currency { get; init; }
 
     public Money(decimal amount, string currency)
     {
         if (amount < 0)
-            throw new ArgumentException("Amount cannot be negative", name of amount);
-        
+            throw new ArgumentException("Amount cannot be negative.");
+
         if (string.IsNullOrWhiteSpace(currency) || currency.Length != 3)
-            throw new ArgumentException("Currency must be a valid 3-letter ISO code", name of currency);
+            throw new ArgumentException("Currency must be a 3-letter ISO code (e.g. USD).");
 
         Amount = Math.Round(amount, 2);
         Currency = currency.ToUpper();
@@ -20,24 +20,24 @@ public record Money
     public Money Add(Money other)
     {
         EnsureSameCurrency(other);
-        return new Money (Amount + other.Amount, Currency)
+        return new Money(Amount + other.Amount, Currency);
     }
 
-    public Money Subtract(other);
+    public Money Subtract(Money other)
     {
         EnsureSameCurrency(other);
         if (Amount < other.Amount)
-            throw new InvalidOperationException("Insufficient funds for this operation");
-        return new Money(Amount - other.Amount, Currency);    
+            throw new InvalidOperationException("Insufficient funds.");
+        return new Money(Amount - other.Amount, Currency);
     }
 
-    public bool isGreaterThan(Money other);
+    public bool IsGreaterThan(Money other)
     {
         EnsureSameCurrency(other);
         return Amount > other.Amount;
     }
 
-        private void EnsureSameCurrency(Money other)
+    private void EnsureSameCurrency(Money other)
     {
         if (Currency != other.Currency)
             throw new InvalidOperationException($"Currency mismatch: {Currency} vs {other.Currency}.");
